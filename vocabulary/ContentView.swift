d43bf4142064445d6f7a5c714ea702672339fa47
shou <vicm510mr@gmail.com>
2025-12-11 16:36:24 +0800

    Initial Commit

---
+++ vocabulary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vocabulary
+//
+//  Created by shou on 2025/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vocabulary/vocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  vocabularyApp.swift
+//  vocabulary
+//
+//  Created by shou on 2025/12/11.
+//
+
+import SwiftUI
+
+@main
+struct vocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
